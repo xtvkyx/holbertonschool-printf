83463f48f3dfa591960be9d8d8b4bb87c0fd3a16
Lamisfaljabli <ljabli@icloud.com>
2025-10-01 19:49:10 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include<stdarg.h>
+#include <stdarg.h>
 /**
-* struct typ - Struct types
-* @types: The type
-* @f: The function associated
-*/
+ * struct typ - Struct for format specifiers
+ * @typ: The format type as a string
+ * @f: Pointer to the function that handles this type
+ */
 typedef struct typ
 {
-	char *typ;
-	int (*f)(va_list);
+   char *typ;
+   int (*f)(va_list);
 } typs;
 
 int _printstr(va_list ap);
