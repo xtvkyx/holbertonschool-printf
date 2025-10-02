84dc54a6f26bd412970d3ddd4a65bfed0dbc5c35
“xtvkyx” <ljabli@icloud.com>
2025-10-02 15:57:33 +0000

    0

--- _printf.c
+++ _printf.c
@@ -1,19 +1,49 @@
-#include "main.h"
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+
+int print_normal(const char *format, int *i)
+{
+	write(1, &format[*i], 1);
+	(*i)++;
+	return (1);
+}
+
+int handle_unknown(char c)
+{
+	write(1, "%", 1);
+	write(1, &c, 1);
+	return (2);
+}
+
+int handle_format(char c, va_list ap)
+{
+	int j;
+	typs typ[] = {
+		{"s", _printstr},
+		{"c", _printchar},
+		{"%", _printperc},
+		{NULL, NULL}
+	};
+
+	for (j = 0; typ[j].typ; j++)
+	{
+		if (c == typ[j].typ[0])
+			return typ[j].f(ap);
+	}
+	return handle_unknown(c);
+}
 
 /**
  * _printf - prints formatted output
- * @format: format string
- * Return: number of characters printed, or -1 if format is NULL
  */
 int _printf(const char *format, ...)
 {
 	int i = 0, counter = 0;
 	va_list ap;
 
-	if (format == NULL)
-		return (-1);
+	if (!format)
+		return (0);
 
 	va_start(ap, format);
 	while (format[i])
@@ -24,10 +54,7 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (!format[i])
-			{
-				va_end(ap);
-				return (-1);
-			}
+				break;
 			counter += handle_format(format[i], ap);
 			i++;
 		}
