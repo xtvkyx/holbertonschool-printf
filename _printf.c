d1dbb14053c1027002c248ccc2d7374849997e92
Lamisfaljabli <ljabli@icloud.com>
2025-10-02 16:00:04 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,52 +6,57 @@
  * @format: format string
  * Return: number of characters printed
  */
-int _printf(const char *format, ...)
+int print_normal(const char *format, int *i)
 {
-int i = 0, j, counter = 0;
-va_list ap;
+	write(1, &format[*i], 1);
+	(*i)++;
+	return (1);
+}
+int handle_unknown(char c)
+{
+	write(1, "%", 1);
+	write(1, &c, 1);
+	return (2);
+}
+int handle_format(char c, va_list ap)
+{
+int j;
 typs typ[] = {
 {"s", _printstr},
 {"c", _printchar},
 {"%", _printperc},
 {NULL, NULL}
 };
-if (format == NULL)
+for (j = 0; typ[j].typ; j++)
+{
+if (c == typ[j].typ[0])
+return typ[j].f(ap);
+}
+return handle_unknown(c);
+}
+/**
+ * _printf - prints formatted output
+ */
+int _printf(const char *format, ...)
+{
+int i = 0, counter = 0;
+va_list ap;
+
+if (!format)
 return (0);
 va_start(ap, format);
 while (format[i])
 {
 if (format[i] != '%')
+counter += print_normal(format, &i);
+else
 {
-write(1, &format[i], 1);
-counter++;
-i++;
-continue;
-}
 i++;
 if (!format[i])
-{
-va_end(ap);
-return (counter);
-}
-
-j = 0;
-while (typ[j].typ)
-{
-if (format[i] == typ[j].typ[0])
-{
-counter += typ[j].f(ap);
 break;
-}
-j++;
-}
-if (!typ[j].typ)
-{
-write(1, "%", 1);
-write(1, &format[i], 1);
-counter += 2;
-}
+counter += handle_format(format[i], ap);
 i++;
+}
 }
 va_end(ap);
 return (counter);
