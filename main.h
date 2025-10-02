e53af6957e5116893af58d7fa465cf2832258adb
Lamisfaljabli <ljabli@icloud.com>
2025-10-02 16:40:13 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
+
 /**
  * struct typ - Struct for format specifiers
  * @typ: The format type as a string
@@ -8,12 +10,13 @@
  */
 typedef struct typ
 {
-   char *typ;
-   int (*f)(va_list);
+	char *typ;
+	int (*f)(va_list);
 } typs;
 
 int _printstr(va_list ap);
 int _printchar(va_list ap);
 int _printperc(va_list ap);
 int _printf(const char *format, ...);
+
 #endif
