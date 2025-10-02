b97c5f6cefad6b6344b712b766353fee2c9e3ee0
Lamisfaljabli <ljabli@icloud.com>
2025-10-02 20:14:32 +0300

    Update print_func.c

--- print_func.c
+++ print_func.c
@@ -3,6 +3,8 @@
 #include "main.h"
 /**
  * _printstr - prints a string from va_list
+ * @ap: A va_list containing the string to print. <--- ADD THIS
+ * Return: The number of characters printed. <--- ADD THIS
  */
 int _printstr(va_list ap)
 {
@@ -23,6 +25,8 @@ int _printstr(va_list ap)
 
 /**
  * _printchar - prints a single char from va_list
+ * @ap: A va_list containing the character to print. <--- ADD THIS
+ * Return: Always 1 (the number of characters printed). <--- ADD THIS
  */
 int _printchar(va_list ap)
 {
@@ -36,6 +40,8 @@ int _printchar(va_list ap)
 
 /**
  * _printperc - prints a percent sign
+ * @ap: A va_list (unused, but required by function pointer signature). <--- ADD THIS
+ * Return: Always 1 (the number of characters printed). <--- ADD THIS
  */
 int _printperc(va_list ap)
 {
