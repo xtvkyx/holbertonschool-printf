3a804d18ec47863da8e00a919b1a3e1ca0ac410b
Lamisfaljabli <ljabli@icloud.com>
2025-10-02 14:59:24 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,58 +9,58 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j, counter = 0;
-	va_list ap;
-	typs typ[] = {
-		{"s", _printstr},
-		{"c", _printchar},
-		{"%", _printperc},
-		{NULL, NULL}
-	};
+    int i = 0, j, counter = 0;
+    va_list ap;
+    typs typ[] = {
+        {"s", _printstr},
+        {"c", _printchar},
+        {"%", _printperc},
+        {NULL, NULL}
+    };
 
-	if (format == NULL)
-		return (0);
+    if (format == NULL)
+        return (0);
+	
+    va_start(ap, format);
 
-	va_start(ap, format);
+    while (format[i])
+    {
+        if (format[i] != '%')
+        {
+            write(1, &format[i], 1);
+            counter++;
+            i++;
+            continue;
+        }
 
-	while (format[i])
-	{
-		if (format[i] != '%')
-		{
-			write(1, &format[i], 1);
-			counter++;
-			i++;
-			continue;
-		}
+        i++;
+        if (!format[i])
+        {
+            va_end(ap);
+            return (counter);
+        }
 
-		i++;
-		if (!format[i])
-		{
-			va_end(ap);
-			return (counter);
-		}
+        j = 0;
+        while (typ[j].typ)
+        {
+            if (format[i] == typ[j].typ[0])
+            {
+                counter += typ[j].f(ap);
+                break;
+            }
+            j++;
+        }
 
-		j = 0;
-		while (typ[j].typ)
-		{
-			if (format[i] == typ[j].typ[0])
-			{
-				counter += typ[j].f(ap);
-				break;
-			}
-			j++;
-		}
+        if (!typ[j].typ)
+        {
+            write(1, "%", 1);
+            write(1, &format[i], 1);
+            counter += 2;
+        }
 
-		if (!typ[j].typ)
-		{
-			write(1, "%", 1);
-			write(1, &format[i], 1);
-			counter += 2;
-		}
+        i++;
+    }
 
-		i++;
-	}
-
-	va_end(ap);
-	return (counter);
+    va_end(ap);
+    return (counter);
 }
