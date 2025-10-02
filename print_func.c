d40406ac7755621af780af169da64a120d091037
Lamisfaljabli <ljabli@icloud.com>
2025-10-02 16:40:47 +0300

    Update print_func.c

--- print_func.c
+++ print_func.c
@@ -1,50 +1,41 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
 /**
-* _printstr - produces string output
-* @ap: argument pointer
-* Description: a function that produces string output.
-* Return: the number of characters printed
-*/
-#include<unistd.h>
-#include<stdio.h>
-#include<stdarg.h>
-#include"main.h"
+ * _printstr - prints a string from va_list
+ */
 int _printstr(va_list ap)
 {
-	int counter = 0;
 	char *str = va_arg(ap, char *);
+	int counter = 0;
 
-	if (str == NULL)
+	if (!str)
 		str = "(null)";
 	while (*str)
 	{
-		write (1, str, 1);
+		write(1, str, 1);
 		str++;
 		counter++;
 	}
 	return (counter);
 }
+
 /**
-* _printchar - produces char output
-* @ap: argument pointer
-* Description: a function that produces char output.
-* Return: the number of characters printed
-*/
+ * _printchar - prints a single char from va_list
+ */
 int _printchar(va_list ap)
 {
 	char c = va_arg(ap, int);
-	write (1, &c, 1);
+	write(1, &c, 1);
 	return (1);
 }
 
 /**
-* _printperc - produces percent output
-* @ap: argument pointer
-* Description: a function that produces percent output.
-* Return: the number of characters printed
-*/
+ * _printperc - prints a percent sign
+ */
 int _printperc(va_list ap)
 {
-	(void)ap;	
-	write (1, "%", 1);
+	(void)ap;
+	write(1, "%", 1);
 	return (1);
 }
