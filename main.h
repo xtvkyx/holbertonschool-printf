397310fd5d7ef5eef1a7d80f9301cc5a5fc9c892
Lamisfaljabli <ljabli@icloud.com>
2025-10-04 01:53:12 +0300

    Update main.h

--- main.h
+++ main.h
@@ -3,26 +3,20 @@
 
 #include <stdarg.h>
 
-<<<<<<< HEAD
 
 int _printint(va_list ap);
 int print_number(long int n);
 int _putchar(char c);
-=======
 int _putchar(char c);
 int _printf(const char *format, ...);
->>>>>>> 711c7f5ac974bb78a3a5e2b73c288a30b696b3eb
 
 int _printstr(va_list ap);
 int _printchar(va_list ap);
 int _printperc(va_list ap);
-<<<<<<< HEAD
 int _printf(const char *format, ...);
-=======
 
 int _printint(va_list ap);
 int print_number(long int n);
->>>>>>> 711c7f5ac974bb78a3a5e2b73c288a30b696b3eb
 
 /**
  * struct types - Struct to map format specifiers to handler functions
