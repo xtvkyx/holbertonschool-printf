b5a10d263fc74b40f8b595d824e360c63ded0f47
Lamisfaljabli <ljabli@icloud.com>
2025-10-04 19:04:58 +0300

    Update print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -4,17 +4,37 @@
 /**
  * _printhex - handles %x (lowercase)
  */
-int _printhex(va_list ap)
+int _printhex(va_list ap, flags_t flags)
 {
 	unsigned int n = va_arg(ap, unsigned int);
-	return (print_unsigned_base(n, 16, 0));
+	int count = 0;
+
+	if (flags.hash && n != 0)
+	{
+		_putchar('0');
+		_putchar('x');
+		count += 2;
+	}
+
+	count += print_unsigned_base(n, 16, 0);
+	return (count);
 }
 
 /**
  * _printHEX - handles %X (uppercase)
  */
-int _printHEX(va_list ap)
+int _printHEX(va_list ap, flags_t flags)
 {
 	unsigned int n = va_arg(ap, unsigned int);
-	return (print_unsigned_base(n, 16, 1));
+	int count = 0;
+
+	if (flags.hash && n != 0)
+	{
+		_putchar('0');
+		_putchar('X');
+		count += 2;
+	}
+
+	count += print_unsigned_base(n, 16, 1);
+	return (count);
 }
