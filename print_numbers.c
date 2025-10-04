7bf6419dce43f054d438449e5a2ecc278c36bfde
Lamisfaljabli <ljabli@icloud.com>
2025-10-04 19:22:10 +0300

    Update print_numbers.c

--- print_numbers.c
+++ print_numbers.c
@@ -3,65 +3,53 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer recursively
- * @n: the integer to print
+ * print_number - prints an unsigned int recursively
+ * @num: the number to print
  * Return: number of characters printed
  */
-int print_number(int n)
+int print_number(unsigned int num)
 {
-	int count = 0;
-	unsigned int num;
+    int count = 0;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		num = -n;
-	}
-	else
-		num = n;
+    if (num / 10)
+        count += print_number(num / 10);
 
-	if (num / 10)
-		count += print_number(num / 10);
-
-	_putchar((num % 10) + '0');
-	count++;
-
-	return (count);
+    count += _putchar((num % 10) + '0');
+    return count;
 }
 
 /**
- * _printint - prints an integer argument
+ * _printint - prints an integer argument with flags
  * @ap: va_list containing the integer
- * @flags: struct with format flags
+ * @flags: flags_t struct containing '+', ' ' flags
  * Return: number of characters printed
  */
 int _printint(va_list ap, flags_t flags)
 {
-	int n = va_arg(ap, int);
-	char sign = 0;
-	unsigned int num;
-	int count = 0;
+    int n = va_arg(ap, int);
+    unsigned int num;
+    int count = 0;
+    char sign = 0;
 
-	if (n < 0)
-	{
-		sign = '-';
-		num = -n;
-	}
-	else
-	{
-		num = n;
-		if (flags.plus)
-			sign = '+';
-		else if (flags.space)
-			sign = ' ';
-	}
+    if (n < 0)
+    {
+        sign = '-';
+        num = (unsigned int)(-(long)n);
+    }
+    else
+    {
+        num = n;
+        if (flags.plus)
+            sign = '+';
+        else if (flags.space)
+            sign = ' ';
+    }
 
-	if (sign)
-		count += _putchar(sign);
+    if (sign)
+        count += _putchar(sign);
 
-	count += print_number(num);
-	return (count);
+    count += print_number(num);
+    return count;
 }
 
 /**
@@ -73,58 +61,61 @@ int _printint(va_list ap, flags_t flags)
  */
 int print_unsigned_base(unsigned int n, int base, int uppercase)
 {
-	char digits[] = "0123456789abcdef";
-	char digits_upper[] = "0123456789ABCDEF";
-	char buffer[32];
-	int i = 0, count = 0;
+    char digits[] = "0123456789abcdef";
+    char digits_upper[] = "0123456789ABCDEF";
+    char buffer[32];
+    int i = 0, count = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	while (n > 0)
-	{
-		if (uppercase)
-			buffer[i++] = digits_upper[n % base];
-		else
-			buffer[i++] = digits[n % base];
-		n /= base;
-	}
+    if (n == 0)
+    {
+        _putchar('0');
+        return 1;
+    }
 
-	while (i--)
-	{
-		_putchar(buffer[i]);
-		count++;
-	}
-	return (count);
+    while (n > 0)
+    {
+        if (uppercase)
+            buffer[i++] = digits_upper[n % base];
+        else
+            buffer[i++] = digits[n % base];
+        n /= base;
+    }
+
+    while (i--)
+    {
+        _putchar(buffer[i]);
+        count++;
+    }
+    return count;
 }
 
 /**
  * _printunsigned - prints %u
- * @ap: va_list
- * @flags: struct with flags (unused)
+ * @ap: va_list containing the unsigned int
+ * @flags: unused flags
+ * Return: number of characters printed
  */
 int _printunsigned(va_list ap, flags_t flags)
 {
-	unsigned int n = va_arg(ap, unsigned int);
-	(void)flags;
-	return (print_unsigned_base(n, 10, 0));
+    unsigned int n = va_arg(ap, unsigned int);
+    (void)flags;
+    return print_unsigned_base(n, 10, 0);
 }
 
 /**
- * _printoctal - prints %o
- * @ap: va_list
- * @flags: struct with flags (# for prefix)
+ * _printoctal - prints %o with '#' flag
+ * @ap: va_list containing the unsigned int
+ * @flags: flags_t struct containing '#'
+ * Return: number of characters printed
  */
 int _printoctal(va_list ap, flags_t flags)
 {
-	unsigned int n = va_arg(ap, unsigned int);
-	int count = 0;
+    unsigned int n = va_arg(ap, unsigned int);
+    int count = 0;
 
-	if (flags.hash && n != 0)
-		count += _putchar('0');
+    if (flags.hash && n != 0)
+        count += _putchar('0');
 
-	count += print_unsigned_base(n, 8, 0);
-	return (count);
+    count += print_unsigned_base(n, 8, 0);
+    return count;
 }
